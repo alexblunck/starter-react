121596018c2f68d2f3a49c2b1635fcf27ec05cca
Alexander Blunck <alex@blunck.xyz>
2016-02-14 22:45:44 +0100

    Webpack: - Seperate styles into their own chunk - Use html-webpack-plugin-remove to exclude style.hash.js file created by using seperate chunk for styles - Fixed other stuff, lazy ... .sry ;) - Gzip compress emitted files - Added comments

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,45 +5,69 @@ import merge from 'webpack-merge'
 import CleanPlugin from 'clean-webpack-plugin'
 import HtmlPlugin from 'html-webpack-plugin'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import CompressionPlugin from 'compression-webpack-plugin'
 import autoprefixer from 'autoprefixer'
+import HtmlPluginRemove from 'html-webpack-plugin-remove'
 
+let config = null;
+
+// ----------------------------------------------------------------------
+// Paths
+// ----------------------------------------------------------------------
 const PATHS = {
     src: path.join(__dirname, 'src'),
+    style: path.join(__dirname, 'src/main.scss'),
     build: path.join(__dirname, 'build')
 };
 
+// ----------------------------------------------------------------------
+// Config: Common
+// ----------------------------------------------------------------------
 const common = {
+    // Allow importing of .jsx files without using file extension
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     entry: {
-        app: PATHS.src
+        app: PATHS.src,
+        style: PATHS.style
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.[hash].js'
+        filename: '[name].js'
     },
     module: {
         loaders: [
-            { test: /.jsx?$/, loaders: ['babel?cacheDirectory'], include: PATHS.src }
+            // Babel Loader
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: PATHS.src,
+                exclude: /node_modules/
+            }
         ]
     },
     plugins: [
+        // Generate index.html file
         new HtmlPlugin({
             template: 'node_modules/html-webpack-template/index.ejs',
             title: 'starter-react',
             appMountId: 'app',
             inject: false,
             minify: { removeComments: true, collapseWhitespace: true }
-        })
+        }),
+        // Remove style.[hash].js file from generated html, which is created
+        // by specifiying a seperate chunk for styles
+        new HtmlPluginRemove(/<script.*?src="style\..*?\.js".*?<\/script>/)
     ],
     postcss: () => [autoprefixer]
 };
 
-let config = null;
-
-// Dev
+// ----------------------------------------------------------------------
+// Config: Development
+// ----------------------------------------------------------------------
 if (argv.dev) {
+    // Set babel environment to "dev" to enable hot module replacement
     process.env.BABEL_ENV = 'dev';
 
     config = merge(common, {
@@ -59,7 +83,13 @@ if (argv.dev) {
         },
         module: {
             loaders: [
-                { test: /.css$/, loaders: ['style', 'css', 'postcss', 'sass'], include: PATHS.src }
+                // Sass Loader
+                {
+                    test: /\.scss$/,
+                    loaders: ['style', 'css', 'postcss', 'sass'],
+                    include: PATHS.src,
+                    exclude: /node_modules/
+                }
             ]
         },
         plugins: [
@@ -68,31 +98,48 @@ if (argv.dev) {
     });
 }
 
-// Build
+// ----------------------------------------------------------------------
+// Config: Build / Production
+// ----------------------------------------------------------------------
 else if (argv.build) {
     config = merge(common, {
+        output: {
+            path: PATHS.build,
+            filename: '[name].[chunkhash].js',
+            chunkFilename: '[id].[chunkhash].js'
+        },
         module: {
             loaders: [
+                // Sass Loader
                 {
-                    test: /.css$/,
-                    loader: ExtractTextPlugin.extract('style', 'css', 'postcss', 'sass'),
-                    include: PATHS.src
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!sass!postcss'),
+                    include: PATHS.src,
+                    exclude: /node_modules/
                 }
             ]
         },
         plugins: [
+            // Clean "build" dir.
             new CleanPlugin([PATHS.build], {
                 verbose: false
             }),
+            // Set node environment to "production" to force react to
+            // remove type checks, etc.
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': '"production"'
             }),
+            new webpack.optimize.OccurrenceOrderPlugin(true),
+            // Minify js
             new webpack.optimize.UglifyJsPlugin({
                 compress: {
                     warnings: false
                 }
             }),
-            new ExtractTextPlugin('[name].[hash].css')
+            // Extract css out of "style" chunk
+            new ExtractTextPlugin('[name].[chunkhash].css'),
+            // Gzip all files in "build" dir.
+            new CompressionPlugin()
         ]
     });
 }
