3840258ea9926b3e21fb17f4c1d61478fd544bea
Alexander Blunck <alex@blunck.co>
2017-08-13 23:04:21 +0200

    Simplified sample application to bare minimum

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,23 @@
 import React from 'react'
-import uuid from 'uuid/v4'
-import Todos from './Todos'
 
 export default class App extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
 
-        this.state = {
-            todos: []
-        }
+        this.state = {}
 
-        this.addTodo = this.addTodo.bind(this)
-        this.editTodo = this.editTodo.bind(this)
-        this.deleteTodo = this.deleteTodo.bind(this)
+        this.sayHello()
     }
 
-    render () {
-        const todos = this.state.todos
-
+    render() {
         return (
             <div>
                 <h1>starter-react</h1>
-
-                {/* Button - Add Todo */}
-                <button onClick={this.addTodo}>Add Todo</button>
-
-                {/* Todos */}
-                <Todos todos={todos} onEdit={this.editTodo} onDelete={this.deleteTodo} />
             </div>
         )
     }
 
-    addTodo () {
-        const id = uuid()
-        const name = 'New Todo'
-
-        this.setState({
-            todos: [...this.state.todos, { id, name }]
-        })
-    }
-
-    editTodo (id, name) {
-        const todos = this.state.todos.map(todo => {
-            if (todo.id === id) {
-                return Object.assign({}, todo, { name })
-            }
-
-            return todo
-        })
-
-        this.setState({todos})
-    }
-
-    deleteTodo (id) {
-        this.setState({
-            todos: this.state.todos.filter(todo => todo.id !== id)
-        })
+    sayHello() {
+        console.log('Hello')
     }
 }
